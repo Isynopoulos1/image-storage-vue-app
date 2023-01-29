5a997e25583336a6d8f113399fa20f9bfdd6cd62
Isela Alarcón <isela.v.alarcon@gmail.com>
2023-01-29 23:02:46 +0100

    the path to upload

--- src/store/modules/images.js
+++ src/store/modules/images.js
@@ -7,12 +7,20 @@ const getters = {
   allImages: (state) => state.images,
 };
 const actions = {
-  async fetchImages({ rootState }) {
+  async fetchImages({ rootState, commit }) {
     const { token } = rootState.auth;
     const response = await api.fetchImages(token);
-    console.log(response);
+    commit("setImages", response.data.data);
+  },
+  async uploadImages({ rootState }, images) {
+    //get the access token
+    const { token } = rootState.auth;
+    //call the api module to do the upload
+    await api.upload(images, token);
+    //redirect our user to ImageList component
   },
 };
+
 const mutations = {
   setImages: (state, images) => {
     state.images = images;
